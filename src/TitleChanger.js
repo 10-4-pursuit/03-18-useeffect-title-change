b57a1a3c3accff61ce6634e5abe3e375782b94e0
athomas227 <andrewthomas@pursuit.org>
2023-12-12 19:30:12 -0500

    finished exercise

---
+++ src/TitleChanger.js
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from 'react';
+
+function TitleChanger() {
+    const [name, setName] = React.useState('');
+    
+    React.useEffect(() => {
+        document.title = 'Hello ' + name;
+    }, [name]);
+
+    return (
+        <div>
+          <h2>Change the Title</h2>
+          <label htmlFor="nameInput">Enter your name: </label>
+          <input type="text" id="nameInput" value={name} onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+      );
+}
+
+export default TitleChanger;
