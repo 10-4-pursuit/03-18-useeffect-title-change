c459d30c1bc730f6284809fd15415e5ddc6e1278
MoniqueC10-4 <monique.charles13@gmail.com>
2024-01-02 00:04:26 -0500

    Add TitleChanger component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TitleChanger from './TitleChanger.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TitleChanger />
+    </div>
+  );
+}
+
+export default App;
