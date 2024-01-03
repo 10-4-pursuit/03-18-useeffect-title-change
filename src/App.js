18dfbacf62a3ca12733b770a39030b2d3739b3ab
KyronGreen6 <kyrongreen@pursuit.org>
2024-01-02 22:17:49 -0500

    Add TitleChanger component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TitleChanger from './TitleChanger';
+
+function App() {
+  return (
+    <div className="App">
+      <TitleChanger/>
+    </div>
+  );
+}
+
+export default App;
