48e10acacefa231eadbb3590f089271adf51d32f
Mikal Din <Mikal Wazeerud-din@pursuit.org>
2024-01-29 09:39:00 -0500

    title changer works i think, not sure where to see title change on browser beside screen once i enter a name

---
+++ 03-18-useeffect-title-chamge/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TitleChanger from './TitleChanger'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <TitleChanger />
+    </div>
+  );
+}
+
+export default App;
---
+++ 03-18-useeffect-title-chamge/src/TitleChanger.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+
+const TitleChanger = () => {
+    const[name, setName] = useState('');
+
+    useEffect(() => {
+        document.title = `Hello, ${name || 'Stranger'}`;
+    },[name]);
+
+    return (
+        <div>
+            <label htmlFor="nameInput">Name:</label>
+            <input 
+            id="nameInput"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            />
+        </div>
+    );
+};
+export default TitleChanger;
+
